ff7b9cfcb28b781c4632813f45173cc5cfe55ac6
nipunjain <nipunjain119@gmail.com>
2021-06-01 02:54:40 +0530

    designing complete

--- app/src/main/java/com/codenipun/scientificcalculator/MainActivity.kt
+++ app/src/main/java/com/codenipun/scientificcalculator/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.codenipun.scientificcalculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
