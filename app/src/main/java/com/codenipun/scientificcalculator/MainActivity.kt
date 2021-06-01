2e245a26c373dc8a013da8c000de135872bea4bd
nipunjain <nipunjain119@gmail.com>
2021-06-01 15:02:41 +0530

    Adding some images

--- app/src/main/java/com/codenipun/scientificcalculator/MainActivity.kt
+++ app/src/main/java/com/codenipun/scientificcalculator/MainActivity.kt
@@ -190,15 +190,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
         btnequals.setOnClickListener {
-//            val str: String = tvpri.text.toString()
+            val str: String = tvpri.text.toString()
 //            // on below line we are calling an evaluate
 //            // method to calculate the value of expressions.
-//            val result: Double = evaluate(str)
+            val result: Double = evaluate(str)
 //            // on below line we are getting result
 //            // and setting it to text view.
-//            val r = result.toString()
-//            tvpri.setText(r)
-//            tvsec.text = str
+            val r = result.toString()
+            tvpri.setText(r)
+            tvsec.text = str
         }
         btnac.setOnClickListener {
             // on clicking on ac button we are clearing
@@ -251,4 +251,128 @@ class MainActivity : AppCompatActivity() {
         return if (n == 1 || n == 0) 1 else n * factorial(n - 1)
     }
 
+    fun evaluate(str: String): Double {
+        return object : Any() {
+            // on below line we ar creating variable
+            // for tracking the position and char pos.
+            var pos = -1
+            var ch = 0
+
+            // below method is for moving to next character.
+            fun nextChar() {
+                // on below line we are incrementing our position
+                // and moving it to next position.
+                ch = if (++pos < str.length) str[pos].toInt() else -1
+            }
+
+            // this method is use to check the extra space
+            // present int the expression and removing it.
+            fun eat(charToEat: Int): Boolean {
+                while (ch == ' '.toInt()) nextChar()
+                // on below line we are checking the char pos
+                // if both is equal then we are returning it to true.
+                if (ch == charToEat) {
+                    nextChar()
+                    return true
+                }
+                return false
+            }
+
+            // below method is to parse our
+            // expression and to get the ans
+            // in this we are calling a parse
+            // expression method to calculate the value.
+            fun parse(): Double {
+                nextChar()
+                val x = parseExpression()
+                if (pos < str.length) throw RuntimeException("Unexpected: " + ch.toChar())
+                return x
+            }
+
+            // in this method we will only perform addition and
+            // subtraction operation on the expression.
+            fun parseExpression(): Double {
+                var x = parseTerm()
+                while (true) {
+                    if (eat('+'.toInt())) x += parseTerm() // addition
+                    else if (eat('-'.toInt())) x -= parseTerm() // subtraction
+                    else return x
+                }
+            }
+
+            // in below method we will perform
+            // only multiplication and division operation.
+            fun parseTerm(): Double {
+                var x = parseFactor()
+                while (true) {
+                    if (eat('*'.toInt())) x *= parseFactor() // multiplication
+                    else if (eat('/'.toInt())) x /= parseFactor() // division
+                    else return x
+                }
+            }
+
+            // below method is use to parse the factor
+            fun parseFactor(): Double {
+                //on below line we are checking for addition
+                // and subtraction and performing unary operations.
+                if (eat('+'.toInt())) return parseFactor() // unary plus
+                if (eat('-'.toInt())) return -parseFactor() // unary minus
+                // creating a double variable for ans.
+                var x: Double
+                // on below line we are creating
+                // a variable for position.
+                val startPos = pos
+                // on below line we are checking
+                // for opening and closing parenthesis.
+                if (eat('('.toInt())) { // parentheses
+                    x = parseExpression()
+                    eat(')'.toInt())
+                } else if (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) {
+                    // numbers
+                    while (ch >= '0'.toInt() && ch <= '9'.toInt() || ch == '.'.toInt()) nextChar()
+                    // on below line we are getting sub string from our string using start and pos.
+                    x = str.substring(startPos, pos).toDouble()
+                } else if (ch >= 'a'.toInt() && ch <= 'z'.toInt()) {
+                    // on below function we are checking for the operator in our expression.
+                    while (ch >= 'a'.toInt() && ch <= 'z'.toInt()) nextChar()
+                    val func = str.substring(startPos, pos)
+                    // calling a method to parse our factor.
+                    x = parseFactor()
+                    // on below line we are checking for square root.
+                    x =
+                        if (func == "sqrt") Math.sqrt(x)
+                        // on below line we are checking for sin function
+                        // and calculating sin function using Math class.
+                        else if (func == "sin") Math.sin(
+                            Math.toRadians(x)
+                            // on below line we are calculating the cos value
+                        ) else if (func == "cos") Math.cos(
+                            Math.toRadians(x)
+                            // on below line we are calculating
+                            // the tan value of our expression.
+                        ) else if (func == "tan")
+                            Math.tan(Math.toRadians(x))
+                        // on below line we are calculating
+                        // log value of the expression.
+                        else if (func == "log")
+                            Math.log10(x)
+                        // on below line we are calculating
+                        // ln value of expression.
+                        else if (func == "ln") Math.log(x)
+                        // f we get any error then
+                        // we simply return the exception.
+                        else throw RuntimeException(
+                            "Unknown function: $func"
+                        )
+                } else {
+                    // if the condition not satisfy then we are returning the exception
+                    throw RuntimeException("Unexpected: " + ch.toChar())
+                }
+                // on below line we are calculating the power of the expression.
+                if (eat('^'.toInt())) x = Math.pow(x, parseFactor()) // exponentiation
+                return x
+            }
+            // at last calling a parse for our expression.
+        }.parse()
+    }
 }
